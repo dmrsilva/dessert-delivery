69402b9c35f417c033a363226759495157f68b0b
Daniel Silva <daniel.mascr@gmail.com>
2022-04-02 17:57:43 -0300

    Backend project created

---
+++ backend/src/test/java/com/myproject/dessertdelivery/DessertdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.dessertdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DessertdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
